f550951de3770afe941fcf43cd9ff4c176938c52
Hemant Kumar Sah <hemantanshu@gmail.com>
2018-08-28 11:09:01 +0530

    added permission and role in ui action

--- src/Models/UIAction.php
+++ src/Models/UIAction.php
@@ -2,6 +2,8 @@
 
 namespace Drivezy\LaravelAdmin\Models;
 
+use Drivezy\LaravelAccessManager\Models\PermissionAssignment;
+use Drivezy\LaravelAccessManager\Models\RoleAssignment;
 use Drivezy\LaravelAdmin\Observers\UIActionObserver;
 use Drivezy\LaravelRecordManager\Models\SystemScript;
 use Drivezy\LaravelUtility\Models\BaseModel;
@@ -15,6 +17,9 @@ class UIAction extends BaseModel {
      * @var string
      */
     protected $table = 'dz_ui_actions';
+    /**
+     * @var array
+     */
     protected $hidden = ['created_by', 'updated_by', 'created_at', 'updated_at', 'deleted_at', 'source_type', 'source_id'];
 
     /**
@@ -36,6 +41,20 @@ class UIAction extends BaseModel {
      */
     public function form () {
         return $this->belongsTo(CustomForm::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function roles () {
+        return $this->hasMany(RoleAssignment::class, 'source_id')->where('source_type', md5(self::class));
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function permissions () {
+        return $this->hasMany(PermissionAssignment::class, 'source_id')->where('source_type', md5(self::class));
     }
 
     /**
